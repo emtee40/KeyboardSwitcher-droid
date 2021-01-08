7517fca89f03b619877c519ea865d35610fa9ae8
Seth <developer.sds100@gmail.com>
2021-01-08 14:45:08 +0000

    Improved opening the dialog by shortcut and by an Intent from another package
    Resolves #5 and #19. The Intent to show the dialog uses the "android.intent.action.VIEW" action and "com.kunzisoft.keyboard.switcher/.KeyboardManagerActivity" as the component. The shortcut starts the activity with this Intent. 3rd party apps can either use the shortcut or use this Intent directly.
    Keyboard Switcher doesn't need permission to draw overlays for this work.

---
+++ app/src/main/java/com/kunzisoft/keyboard/switcher/CreateShortcutActivity.java
@@ -0,0 +1,45 @@
+package com.kunzisoft.keyboard.switcher;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.pm.ShortcutInfoCompat;
+import androidx.core.content.pm.ShortcutManagerCompat;
+import androidx.core.graphics.drawable.IconCompat;
+
+/**
+ * Activity to create a shortcut to show the Keyboard Switcher.
+ */
+public class CreateShortcutActivity extends AppCompatActivity {
+
+    private static final String SHORTCUT_ID = "show_keyboard_switcher";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String shortcutLabel = getString(R.string.shortcut_short_label_show_keyboard_switcher);
+
+        IconCompat shortcutIcon =
+                IconCompat.createWithResource(this, R.drawable.ic_keyboard_white_32dp);
+
+        Intent intent = new Intent(this, KeyboardManagerActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+
+        ShortcutInfoCompat shortcutInfo = new ShortcutInfoCompat.Builder(this, SHORTCUT_ID)
+                .setIcon(shortcutIcon)
+                .setShortLabel(shortcutLabel)
+                .setIntent(intent)
+                .build();
+
+        Intent shortcutIntentResult =
+                ShortcutManagerCompat.createShortcutResultIntent(this, shortcutInfo);
+
+        setResult(Activity.RESULT_OK, shortcutIntentResult);
+        finish();
+    }
+}
